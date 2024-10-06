d70fda19df4588a3f00dd5ad4be80d901f503321
Vitaly Vegera <surewek@gmail.com>
2024-10-06 23:16:00 +0300

    feat: add streams tasks solutions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import  { Transform } from 'stream'
+
 const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, `${chunk
+                .toString()
+                .split('')
+                .reverse()
+                .join('')
+                .replace(/^\n/, '')}\n`);
+        },
+    });
+
+    process.stdin.pipe(reverse).pipe(process.stdout);
 };
 
 await transform();
