6ff122b590cdc75b4f525b3636f41041e70569d2
Vitaly Vegera <surewek@gmail.com>
2024-10-06 22:30:08 +0300

    feat: add cli tasks solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,24 @@
+const START_USER_ARGS_INDEX = 2;
+const ARGS_PREFIX = '--';
+const ARGS_SEPARATOR = ', ';
+
+const parceArgPair = (key, value) => `${key} is ${value}`;
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+
+    let parcedArgs = [];
+
+    for (let i = START_USER_ARGS_INDEX; i < args.length; i += 2) {
+        const key = args[i].replace(ARGS_PREFIX, '');
+        const value = args[i + 1];
+
+        const parcedArgPair = parceArgPair(key, value);
+
+        parcedArgs.push(parcedArgPair);
+    }
+
+    process.stdout.write(parcedArgs.join(ARGS_SEPARATOR));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,15 @@
+const RSS_PREFIX = 'RSS_';
+const VARS_SEPARATOR = '=';
+const PAIR_SEPARATOR = ', ';
+
 const parseEnv = () => {
-    // Write your code here 
+    const parsedVars = Object
+        .entries(process.env)
+        .filter(([key, value]) => key.startsWith(RSS_PREFIX))
+        .map(item => item.join(VARS_SEPARATOR))
+        .join(PAIR_SEPARATOR);
+
+    process.stdout.write(parsedVars);
 };
 
 parseEnv();
